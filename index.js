d2d8a0f413e01aeab24b6799beb99546d321d499
Katie <katiefaybehrmann@gmail.com>
2022-10-31 16:44:26 -0600

    commit tree 5ccb8b05a92233436e51c608a9a3a1742f9965d5

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+let currentUser ="Grace Hopper";
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+let shortGreeting = `Welcome, ${firstInitial}!`;
